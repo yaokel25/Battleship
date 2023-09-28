bca7d3790da2a40295534e7bdf09b7478b703ff5
yaokel25 <yaokel25@7hills.org>
2023-09-28 12:58:08 -0400

    started placing ships

--- Board.java
+++ Board.java
@@ -8,11 +8,29 @@ public class Board{
         
         //print all elements of 2d array
         public void printBoard(){
-            for(int j = 0; j <= numCols; j++){
-            for (int i = 0; i <= numRows; i++){
+            for(int j = 0; j < numCols; j++){
+            for (int i = 0; i < numRows; i++){
             System.out.print(gameBoard[j][i]);
             }
             System.out.print("\n");
         }
     }
+    
+    //setup method
+    public void placeShip(){
+        int s1C = (int) (Math.random()*10);
+        int s1R = (int) (Math.random()*11);
+        gameBoard [s1R][s1C] = 1;
+        gameBoard [s1R][s1C +1] = 1;
+
+        int s1C = (int) (Math.random()*10);
+        int s1R = (int) (Math.random()*11);
+        gameBoard [s1R][s1C] = 1;
+        gameBoard [s1R][s1C +1] = 1;
+
+
+
+        
+    }
+    //play method
 }
--- Tester.java
+++ Tester.java
@@ -1,8 +1,10 @@
 public class Tester{
 
     public static void main(String[] args){
-    Board b1 = new Board;
+    Board b1 = new Board();
     b1.printBoard();
+    
+
     
     }
 
