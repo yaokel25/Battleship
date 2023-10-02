ce63d955eb2eb7a2f52805f11259e2dc2bc61129
yaokel25 <yaokel25@7hills.org>
2023-10-02 08:23:08 -0400

    made user imput for ship location

--- Board.java
+++ Board.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class Board{
 
         int numRows = 10;
         int numCols = 10;
         int[] ships = new int[5]; // {2,3,3,4,5}
+        Scanner scan = new Scanner(System.in); 
 
         int[][] gameBoard = new int[numRows][numCols];
         
@@ -18,7 +20,7 @@ public class Board{
     
     //setup method
     public void placeShip(){
-        int sOneC = (int) (Math.random()*10);
+        /*int sOneC = (int) (Math.random()*10);
         int sOneR = (int) (Math.random()*11);
         gameBoard [sOneR][sOneC] = 1;
         gameBoard [sOneR][sOneC +1] = 1;
@@ -29,11 +31,21 @@ public class Board{
         while (sTwoC != sOneC && sTwoC != (sTwoC + 1))
         
         gameBoard [sTwoR][sTwoC] = 1;
-        gameBoard [sOneR][sOneC +1] = 1;
-
-
-
+        gameBoard [sOneR][sOneC +1] = 1;*/
+        System.out.println("Enter the location for your first ship (2 blocks): ");
+        String ship1 = scan.nextLine();
+        System.out.println("Enter the location for your second ship (3 blocks): ");
+        String ship2 = scan.nextLine();
+        System.out.println("Enter the location for your third ship (3 blocks): ");
+        String ship3 = scan.nextLine();
+        System.out.println("Enter the location for your fourth ship (4 blocks): ");
+        String ship4 = scan.nextLine();
+        System.out.println("Enter the location for your fifth ship (5 blocks): ");
+        String ship5 = scan.nextLine();
         
-    }
+
+
+
+        }
     //play method
 }
